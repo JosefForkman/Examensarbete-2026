792de30970f6d73b66b44dbdc2c64d03a1d0d65c
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-08 10:51:37 +0200

    Added connection between backend and database

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,4 +1,13 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("mydb")
+    ?? throw new InvalidOperationException("Connection string 'mydb' not found.");
+
+builder.Services.AddDbContextFactory<Backend.Data.DbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.AddGraphQL().AddTypes();
 

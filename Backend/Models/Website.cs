43d3b8166cbfa999740f5583955b61cdf75e2506
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-08 09:47:02 +0200

    Added Website table model

---
+++ Backend/Models/Website.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Website
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string SiteName { get; set; } = string.Empty;
+        [Required]
+        [Url]
+        public string RSSUrl { get; set; } = string.Empty;
+        [Required]
+        [Url]
+        public string SiteUrl { get; set; } = string.Empty;
+    }
+}

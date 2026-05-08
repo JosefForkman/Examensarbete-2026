b0bacac99156237bf9a68b17e1326989d276003b
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-08 09:51:45 +0200

    Added PostItem table model

---
+++ Backend/Models/PostItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class PostItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        [Required]
+        public string Link { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int WebsiteId { get; set; }
+
+        //Navigational properties
+        public Website Website { get; set; } = null!;
+    }
+}

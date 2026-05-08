2d3e5852a96bd06c4f32a83cf2663d3d39289a6b
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-08 10:12:53 +0200

    Added Watched table model

---
+++ Backend/Models/Watched.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Watched
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int PostItemId { get; set; }
+
+        // Navigational properties
+        public IdentityUser User { get; set; } = null!;
+        public PostItem PostItem { get; set; } = null!;
+    }
+}

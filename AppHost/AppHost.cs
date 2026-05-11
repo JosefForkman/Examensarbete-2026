238ca78423f89ea5d956de1e0cad75b5a17528e5
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-11 13:32:25 +0200

    Made it so that postgres is always run one same port

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,6 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
+var postgres = builder.AddPostgres("postgres")
+    .WithHostPort(5432)
+    .WithLifetime(ContainerLifetime.Persistent);
 var db = postgres.AddDatabase("mydb");
 
 builder.AddProject<Projects.Backend>("backend")

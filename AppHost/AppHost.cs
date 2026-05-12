4273cb4cf749f6731429ed7217dc03c650db06ef
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2026-05-12 08:55:22 +0200

    Refactor database setup in AppHost and Program files to enhance clarity and structure

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,6 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
+var postgres = builder
+                .AddPostgres("postgres")
+                .WithHostPort(5432)
+                .WithDataVolume()
+                .WithLifetime(ContainerLifetime.Persistent)
+                .WithPgAdmin();
 var db = postgres.AddDatabase("mydb");
 
 builder.AddProject<Projects.Backend>("backend")
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,15 +3,29 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("mydb")
-    ?? throw new InvalidOperationException("Connection string 'mydb' not found.");
+// var connectionString = builder.Configuration.GetConnectionString("mydb")
+//     ?? throw new InvalidOperationException("Connection string 'mydb' not found.");
 
-builder.Services.AddDbContextFactory<Backend.Data.DbContext>(options =>
-    options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<RSSDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("mydb")
+        ?? throw new InvalidOperationException("Connection string 'mydb' not found.");
+
+    options.UseNpgsql(connectionString);
+});
+
+builder.EnrichNpgsqlDbContext<RSSDbContext>();
 
 builder.AddGraphQL().AddTypes();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<RSSDbContext>();
+    await context.Database.MigrateAsync();
+}
 
 app.MapGraphQL();
 

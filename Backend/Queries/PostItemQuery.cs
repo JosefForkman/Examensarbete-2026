f81c57a6d4aa4b574f48367cae2c1a22f889a736
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-13 15:15:50 +0200

    Created query to get all postItems

---
+++ Backend/Queries/PostItemQuery.cs
@@ -0,0 +1,26 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Backend.Service;
+using Backend.Types;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Queries
+{
+    [QueryType]
+    public static class PostItemQuery
+    {
+        public static IQueryable<PostItemType> GetPostItems(PostItemService postItemService)
+        {
+            return postItemService.GetAll()
+                .Select(p => new PostItemType
+                {
+                    Title = p.Title,
+                    Description = p.Description,
+                    Link = p.Link,
+                    ImageUrl = p.ImageUrl,
+                    PublicationDate = p.PublicationDate,
+                    WebsiteName = p.Website.SiteName
+                });
+        }
+    }
+}

0d90794969a23a44df57c4cf8c27b3b5f43ae850
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-13 14:59:50 +0200

    Made methods virtual to allow for editing in classes that inherits from this class

--- Backend/Service/GenericService.cs
+++ Backend/Service/GenericService.cs
@@ -7,14 +7,14 @@ public class GenericService<T>(RSSDbContext context) : IGenericService<T> where 
 {
     protected readonly DbSet<T> _dbSet = context.Set<T>();
 
-    public async Task<T> CreateAsync(T entity)
+    public virtual async Task<T> CreateAsync(T entity)
     {
         await _dbSet.AddAsync(entity);
         await context.SaveChangesAsync();
         return entity;
     }
 
-    public async Task DeleteAsync(int id)
+    public virtual async Task DeleteAsync(int id)
     {
         var entity = await GetByIdAsync(id);
         if (entity is not null)
@@ -24,17 +24,17 @@ public class GenericService<T>(RSSDbContext context) : IGenericService<T> where 
         }
     }
 
-    public IQueryable<T> GetAll()
+    public virtual IQueryable<T> GetAll()
     {
         return _dbSet.AsNoTracking();
     }
 
-    public async Task<T?> GetByIdAsync(int id)
+    public virtual async Task<T?> GetByIdAsync(int id)
     {
         return await _dbSet.FindAsync(id);
     }
 
-    public async Task<T> UpdateAsync(T entity)
+    public virtual async Task<T> UpdateAsync(T entity)
     {
         _dbSet.Update(entity);
         await context.SaveChangesAsync();

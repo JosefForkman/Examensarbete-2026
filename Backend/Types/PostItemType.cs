4bd724f52dd6dc97afbdcc9366db7bb899409ac6
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-13 14:29:08 +0200

    Created a PostItemType for use in hotchocolate

---
+++ Backend/Types/PostItemType.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Types
+{
+    public class PostItemType
+    {
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string Link { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int WebsiteName { get; set; }
+    }
+}

37927bfc534dcd159738276dca731afcf07d6de5
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-14 11:33:37 +0200

    Created payload for use when creating postitem

---
+++ Backend/Types/CreatePostItemPayload.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Types
+{
+    public class CreatePostItemPayload
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string Link { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int WebsiteId { get; set; }
+    }
+}

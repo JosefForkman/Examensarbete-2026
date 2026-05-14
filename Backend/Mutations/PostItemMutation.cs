84be9de15a45f8eb6b0bb7623adca4ba28c0abdd
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-14 11:40:01 +0200

    Created mutation for use when creating a postitem

---
+++ Backend/Mutations/PostItemMutation.cs
@@ -0,0 +1,43 @@
+﻿using Backend.Models;
+using Backend.Service;
+using Backend.Types;
+
+namespace Backend.Mutations
+{
+    [MutationType]
+    public class PostItemMutation
+    {
+        public async Task<CreatePostItemPayload> CreatePostItem(CreatePostItemInput input, [Service] IGenericService<PostItem> postItemService, [Service] WebsiteService websiteService)
+        {
+            var website = await websiteService.GetByUrlAsync(input.WebsiteUrl);
+
+            if (website == null)
+            {
+                throw new Exception($"Website with URL '{input.WebsiteUrl}' not found.");
+            }
+
+            var postItem = new PostItem
+            {
+                Title = input.Title,
+                Description = input.Description,
+                Link = input.Link,
+                ImageUrl = input.ImageUrl,
+                PublicationDate = input.PublicationDate,
+                WebsiteId = website.Id
+            };
+
+            var createdPostItem = await postItemService.CreateAsync(postItem);
+
+            return new CreatePostItemPayload
+            {
+                Id = createdPostItem.Id,
+                Title = createdPostItem.Title,
+                Description = createdPostItem.Description,
+                Link = createdPostItem.Link,
+                ImageUrl = createdPostItem.ImageUrl,
+                PublicationDate = createdPostItem.PublicationDate,
+                WebsiteId = createdPostItem.WebsiteId
+            };
+        }
+    }
+}

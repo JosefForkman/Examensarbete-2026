88a20a37ebdddbb15fe38de9db4fd2ddfaf39540
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-14 11:52:06 +0200

    Created update postitem input type

---
+++ Backend/Types/UpdatePostItemInput.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Types
+{
+    public class UpdatePostItemInput
+    {
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public string Link { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string WebsiteUrl { get; set; } = null!;
+    }
+}

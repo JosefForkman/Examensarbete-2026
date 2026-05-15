22afaca1ff6ceeff9e40744f67f28a54407219ba
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-15 09:23:29 +0200

    Created Type for watched

---
+++ Backend/Types/WatchedType.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Types
+{
+    public class WatchedType
+    {
+        public int Id { get; set; }
+        public int PostItemId { get; set; }
+        public int UserId { get; set; }
+    }
+}

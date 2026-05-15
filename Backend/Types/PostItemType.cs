080cff789de9d63eb50fc8e989a67c975a39f5d3
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-15 09:07:54 +0200

    Added id to type

--- Backend/Types/PostItemType.cs
+++ Backend/Types/PostItemType.cs
@@ -2,6 +2,7 @@
 {
     public class PostItemType
     {
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string Link { get; set; } = string.Empty;

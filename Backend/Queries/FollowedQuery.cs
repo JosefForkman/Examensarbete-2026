53f6c39566fb8b0bcd1f66c3ce85e053071eba1d
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 09:59:23 +0200

    Implemented GetFollowedItems query

---
+++ Backend/Queries/FollowedQuery.cs
@@ -0,0 +1,25 @@
+﻿using Backend.Models;
+using Backend.Service;
+using Backend.Types;
+
+namespace Backend.Queries
+{
+    [QueryType]
+    public static class FollowedQuery
+    {
+        [UsePaging]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public  static IQueryable<FollowedType> GetFollowedItems([Service] GenericService<Followed> followedService)
+        {
+            return followedService.GetAll()
+                .Select(f => new FollowedType
+                {
+                    Id = f.Id,
+                    UserId = f.UserId,
+                    WebsiteId = f.WebsiteId
+                });
+        }
+    }
+}

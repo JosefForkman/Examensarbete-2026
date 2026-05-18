9d5bfee7d44a66f8c419f7e4be855ac2547b0888
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 10:30:56 +0200

    Implemented CreatedFollowed mutation

---
+++ Backend/Mutations/FollowedMutation.cs
@@ -0,0 +1,28 @@
+﻿using Backend.Models;
+using Backend.Service;
+using Backend.Types.Followed;
+
+namespace Backend.Mutations
+{
+    [MutationType]
+    public class FollowedMutation
+    {
+        public async Task<CreateFollowedPayload> CreateFollowed(CreateFollowedInput input, [Service] GenericService<Followed> followedService)
+        {
+            var followed = new Followed
+            {
+                UserId = input.UserId,
+                WebsiteId = input.WebsiteId
+            };
+
+            var createdFollowed = await followedService.CreateAsync(followed);
+
+            return new CreateFollowedPayload
+            {
+                Id = createdFollowed.Id,
+                UserId = createdFollowed.UserId,
+                WebsiteId = createdFollowed.WebsiteId
+            };
+        }
+    }
+}

391278ba0ffa6aa172829fd2e43188170cefb953
Josef Forkman <josef@forkman.dev>
2026-05-18 15:42:21 +0200

    Remove error handling attributes from CreateAsync, UpdateAsync, and DeleteAsync methods in IGenericService

--- Backend/Service/IGenericService.cs
+++ Backend/Service/IGenericService.cs
@@ -24,7 +24,6 @@ public interface IGenericService<T> where T : class
     /// <param name="entity">The entity to create.</param>
     /// <param name="cancellationToken">Allows the create operation to be canceled.</param>
     /// <returns>The created entity with an updated ID.</returns>
-    [Error<ValidationException>]
     Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default);
 
     /// <summary>
@@ -33,8 +32,6 @@ public interface IGenericService<T> where T : class
     /// <param name="entity">The entity with updated values.</param>
     /// <param name="cancellationToken">Allows the update operation to be canceled.</param>
     /// <returns>The updated entity.</returns>
-    [Error<ValidationException>]
-    [Error<NotFoundException>]
     Task<T> UpdateAsync(T entity, CancellationToken cancellationToken = default);
 
     /// <summary>
@@ -43,6 +40,5 @@ public interface IGenericService<T> where T : class
     /// <param name="id">The ID of the entity to delete.</param>
     /// <param name="cancellationToken">Allows the delete operation to be canceled.</param>
     /// <returns>A task representing the asynchronous operation.</returns>
-    [Error<NotFoundException>]
     Task DeleteAsync(int id, CancellationToken cancellationToken = default);
 }

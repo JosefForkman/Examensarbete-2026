7b3c56586c645a3cb70892c838b64834903c766c
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 09:07:50 +0200

    Implemented GetWatchedItems query

---
+++ Backend/Queries/WatchedQuery.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Models;
+using Backend.Service;
+using Backend.Types;
+
+namespace Backend.Queries
+{
+    [QueryType]
+    public static class WatchedQuery
+    {
+        public static IQueryable<WatchedType> GetWatchedItems([Service] GenericService<Watched> watchedService)
+        {
+            return watchedService.GetAll()
+                .Select(w => new WatchedType
+                {
+                    Id = w.Id,
+                    PostItemId = w.PostItemId,
+                    UserId = w.UserId
+                });
+        }
+    }
+}

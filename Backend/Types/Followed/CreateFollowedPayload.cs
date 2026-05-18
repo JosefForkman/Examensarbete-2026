eecde3a5a742e008fdeb0eb9dfed7888d2f37794
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 10:21:07 +0200

    Created createFollowedPayload for creating new Followed item

---
+++ Backend/Types/Followed/CreateFollowedPayload.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Types.Followed
+{
+    public class CreateFollowedPayload
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = null!;
+        public int WebsiteId { get; set; }
+    }
+}

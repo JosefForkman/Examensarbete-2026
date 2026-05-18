f93c9ee104197cfa8fb1de13fa20b814d1b5d2f4
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 09:26:35 +0200

    Made UserId not nullable

--- Backend/Types/Watched/WatchedType.cs
+++ Backend/Types/Watched/WatchedType.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public int PostItemId { get; set; }
-        public string UserId { get; set; }
+        public string UserId { get; set; } = null!;
     }
 }

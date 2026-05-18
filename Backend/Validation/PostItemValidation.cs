30c08df8f93a9cade470ab2c55d51a8c4b9eafdd
Josef Forkman <josef@forkman.dev>
2026-05-18 15:29:35 +0200

    Add FluentValidation for Followed, PostItem, Watched, and Website models

---
+++ Backend/Validation/PostItemValidation.cs
@@ -0,0 +1,24 @@
+using Backend.Models;
+using FluentValidation;
+
+namespace Backend.Validation;
+
+public class PostItemValidation : AbstractValidator<PostItem>
+{
+    public PostItemValidation()
+    {
+        RuleSet("Create", () =>
+        {
+            RuleFor(item => item.Title).NotEmpty().MaximumLength(200);
+            RuleFor(item => item.Link).NotEmpty().Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute));
+            RuleFor(item => item.WebsiteId).GreaterThan(0);
+        });
+
+        RuleSet("Update", () =>
+        {
+            RuleFor(item => item.Title).MaximumLength(200).When(i => !string.IsNullOrEmpty(i.Title));
+            RuleFor(item => item.Link).Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute)).When(i => !string.IsNullOrEmpty(i.Link));
+        });
+    }
+}
+

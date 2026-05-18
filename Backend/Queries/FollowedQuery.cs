0d585b8378aac3852ff07c6c61bb87f7d87b1c51
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 10:06:42 +0200

    Implemented GetFollowedItemById Query

--- Backend/Queries/FollowedQuery.cs
+++ Backend/Queries/FollowedQuery.cs
@@ -11,7 +11,7 @@ namespace Backend.Queries
         [UseProjection]
         [UseFiltering]
         [UseSorting]
-        public  static IQueryable<FollowedType> GetFollowedItems([Service] GenericService<Followed> followedService)
+        public static IQueryable<FollowedType> GetFollowedItems([Service] GenericService<Followed> followedService)
         {
             return followedService.GetAll()
                 .Select(f => new FollowedType
@@ -21,5 +21,21 @@ namespace Backend.Queries
                     WebsiteId = f.WebsiteId
                 });
         }
-    }
+
+        public static async Task<FollowedType?> GetFollowedItemById(int id, [Service] GenericService<Followed> followedService)
+        {
+            var followed = await followedService.GetByIdAsync(id);
+
+            if (followed == null)
+            {
+                return null;
+            }
+
+            return new FollowedType
+            {
+                Id = followed.Id,
+                UserId = followed.UserId,
+                WebsiteId = followed.WebsiteId
+            };
+        }
 }

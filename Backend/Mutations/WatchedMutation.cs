e387cfc21d3a0de720a65ac5f9c175a872a9a8e4
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 09:23:39 +0200

    implemented CreateWatched mutatation

---
+++ Backend/Mutations/WatchedMutation.cs
@@ -0,0 +1,28 @@
+﻿using Backend.Models;
+using Backend.Service;
+using Backend.Types.Watched;
+
+namespace Backend.Mutations
+{
+    [MutationType]
+    public class WatchedMutation
+    {
+        public async Task<CreateWatchedPayload> CreateWatched(CreateWatchedInput input, [Service] GenericService<Watched> watchedService)
+        {
+            var watched = new Watched
+            {
+                PostItemId = input.PostItemId,
+                UserId = input.UserId
+            };
+
+            var createdWatched = await watchedService.CreateAsync(watched);
+
+            return new CreateWatchedPayload
+            {
+                Id = createdWatched.Id,
+                PostItemId = createdWatched.PostItemId,
+                UserId = createdWatched.UserId
+            };
+        }
+    }
+}

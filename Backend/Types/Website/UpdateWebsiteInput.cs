8938c02583db0f4bc7adb65d471713992920f3d3
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 09:38:54 +0200

    Created updateWebsiteInput for updating a website

---
+++ Backend/Types/Website/UpdateWebsiteInput.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Types.Website
+{
+    public class UpdateWebsiteInput
+    {
+        public string SiteName { get; set; } = null!;
+        public string RSSUrl { get; set; } = null!;
+        public string SiteUrl { get; set; } = null!;
+    }
+}

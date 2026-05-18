29111dba470cac3da5bf7f919a35fbea4e00b417
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-18 10:34:33 +0200

    Created updateFollowedPayload for updating a Followed item

---
+++ Backend/Types/Followed/UpdateFollowedPayload.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Types.Followed
+{
+    public class UpdateFollowedPayload
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = null!;
+        public int WebsiteId { get; set; }
+    }
+}

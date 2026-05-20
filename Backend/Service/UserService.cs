93f20d565596f918f821232a8d22b40c11859b56
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-20 11:29:04 +0200

    Created new service to be able to get a user by id

---
+++ Backend/Service/UserService.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Data;
+using Backend.Models;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+
+namespace Backend.Service
+{
+    public class UserService(RSSDbContext context, IValidator<IdentityUser> validator) 
+        : GenericService<IdentityUser>(context, validator)
+    {
+        public async Task<IdentityUser?> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}

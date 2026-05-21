b7f31a79ec6668da3fbf2ad09591492e4c2a98b5
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-21 10:57:35 +0200

    Added new project that handles reading RSSfeed and saves info

---
+++ RSSFeedReader/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace RSSFeedReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

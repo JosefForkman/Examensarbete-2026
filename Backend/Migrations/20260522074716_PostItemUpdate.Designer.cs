b8ae7f95b161577c15a736b5f40861ed018b1115
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-22 09:49:42 +0200

    Added new migration for the updated postitem

--- Backend/Migrations/20260522074716_PostItemUpdate.Designer.cs
+++ Backend/Migrations/20260522074716_PostItemUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations
 {
     [DbContext(typeof(RSSDbContext))]
-    partial class RSSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260522074716_PostItemUpdate")]
+    partial class PostItemUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

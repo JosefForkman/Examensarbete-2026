039f52d52924c0145b918faca354513d46c395e9
Josef Forkman <josef@forkman.dev>
2026-05-27 16:40:14 +0200

    move audio types to a new file and update AudioContext implementation

--- frontend/src/context/AudioContext.tsx
+++ frontend/src/context/AudioContext.tsx
@@ -1,50 +1,28 @@
 "use client";
 import { createContext, useContext, useState } from "react";
-import z from "zod";
-
-const AudioTrack = z.object({
-    id: z.string(),
-    title: z.string(),
-    artist: z.string(),
-    url: z.url(),
-    provider: z.enum(["rss", "spotify", "youtube"]),
-    durationInSeconds: z.number().default(0),
-});
-
-type AudioTrack = z.infer<typeof AudioTrack>;
-
-const AudioContextType = z.object({
-    currentTrack: AudioTrack.nullable(),
-    queue: z.array(AudioTrack),
-    isPlaying: z.boolean(),
-    currentTime: z.number(),
-    playTrack: z.function({ input: [AudioTrack], output: z.void() }),
-    addToQueue: z.function({ input: [AudioTrack], output: z.void() }),
-    togglePlay: z.function({ input: [], output: z.void() }),
-    skipNext: z.function({ input: [], output: z.void() }),
-});
-
-type AudioContextType = z.infer<typeof AudioContextType>;
+import { AudioContextType, AudioTrack } from "@/types/Audio";
 
 const AudioContext = createContext<AudioContextType | undefined>(undefined);
 
 export const AudioProvider = ({ children }: { children: React.ReactNode }) => {
     const [currentTrack, setCurrentTrack] = useState<AudioTrack | null>(null);
     const [queue, setQueue] = useState<AudioTrack[]>([]);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentTime, setCurrentTime] = useState(0);
 
     return (
         <AudioContext.Provider
             value={{
                 currentTrack,
                 queue,
-                isPlaying,
-                currentTime,
-                playTrack: () => {},
-                addToQueue: () => {},
-                togglePlay: () => {},
-                skipNext: () => {},
+                addToQueue: (track) => setQueue((prev) => [track, ...prev]),
+                updateQueue: (newQueue) => setQueue(newQueue),
+                deleteFromQueue: (trackId) => {
+                    setQueue((prev) =>
+                        prev.filter((track) => track.id !== trackId),
+                    );
+                },
+                setCurrentTrack: (track) => {
+                    setCurrentTrack(track);
+                },
             }}>
             {children}
         </AudioContext.Provider>

8c4196750bc8cbd0ca8bbc840663948599bf353b
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-28 14:48:56 +0200

    Created validation file for UpdateWatchedInput

---
+++ Backend/Validation/WatchedValidations/UpdateWatchedInputValidation.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Types.Watched;
+using FluentValidation;
+
+namespace Backend.Validation.WatchedValidations
+{
+    public class UpdateWatchedInputValidation : AbstractValidator<UpdateWatchedInput>
+    {
+        public UpdateWatchedInputValidation()
+        {
+            RuleSet("Update", () =>
+            {
+                RuleFor(input => input.UserId).NotEmpty().WithMessage("UserId is required.");
+                RuleFor(input => input.PostItemId).GreaterThan(0).WithMessage("PostItemId must be greater than 0.");
+            });
+        }
+    }
+}

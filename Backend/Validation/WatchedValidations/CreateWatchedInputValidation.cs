db5468ef7d5cf8103c4912aab9513fa92aae43b8
darkdusk234 <johan.svensson.01@hotmail.com>
2026-05-28 14:46:28 +0200

    Created validation file for CreateWatchedInput

---
+++ Backend/Validation/WatchedValidations/CreateWatchedInputValidation.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Types.Watched;
+using FluentValidation;
+
+namespace Backend.Validation.WatchedValidations
+{
+    public class CreateWatchedInputValidation : AbstractValidator<CreateWatchedInput>
+    {
+        public CreateWatchedInputValidation()
+        {
+            RuleSet("Create", () =>
+            {
+                RuleFor(input => input.UserId).NotEmpty().WithMessage("UserId is required.");
+                RuleFor(input => input.PostItemId).GreaterThan(0).WithMessage("PostItemId must be greater than 0.");
+            });
+        }
+    }
+}

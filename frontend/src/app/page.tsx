6022b1072fa711c3df37067a7d5b396cf3d66e4a
Josef Forkman <josef@forkman.dev>
2026-06-03 21:21:43 +0200

    integrate Card component and mock post items in Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,45 @@
-export default function Home() {
+import Card from "@/component/Card";
+import { client, graphql } from "@/gql";
+
+export default async function Home() {
+    const postItemsQuery = graphql(`
+        query {
+            postItems {
+                nodes {
+                    id
+                    title
+                    imageUrl
+                }
+            }
+        }
+    `);
+
+    // const { postItems } = await client().request(postItemsQuery);
+
+    type postItems = {
+        id: number;
+        title: string;
+        imageUrl: string | null;
+        publicationDate: string;
+    };
+
+    const postItems = [
+        {
+            id: 1,
+            title: "Syntax - Tasty Web Development Treats",
+            // imageUrl: null,
+            imageUrl: "https://megaphone.imgix.net/podcasts/5197fe5a-42f7-11f0-affd-87d9985a1760/image/c86a54acd72683732c4773e25bf0ae14.png?ixlib=rails-4.3.1&max-w=3000&max-h=3000&fit=crop&auto=format,compress",
+            publicationDate: "2026-06-01T11:00:00.000Z",
+        },
+        {
+            id: 2,
+            imageUrl:
+                "https://static-cdn.sr.se/images/2519/53582c25-5165-432c-a16f-6b51b964d269.jpg?preset=api-itunes-presentation-image",
+            title: "P3 Dokumentär",
+            publicationDate: "2026-06-01T11:00:00.000Z",
+        },
+    ] satisfies postItems[];
+
     return (
         <main>
             <h1>Welcome to the Audio Player App</h1>
@@ -6,6 +47,16 @@ export default function Home() {
                 Explore your music collection with our sleek and intuitive audio
                 player.
             </p>
+            {postItems &&
+                postItems.map((postItem) => (
+                    <Card
+                        key={postItem.id}
+                        id={postItem.id}
+                        imageUrl={postItem.imageUrl}
+                        title={postItem.title}
+                        publicationDate={postItem.publicationDate}
+                    />
+                ))}
         </main>
     );
 }
